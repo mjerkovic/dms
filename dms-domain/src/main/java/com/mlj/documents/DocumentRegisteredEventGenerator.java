17093d847dfeb326ac6b979291cbea68bc9a48b0
Mark Jerkovic <mjerkovic@aconex.com>
2012-12-09 23:19:11 +1100

    Remove unused class.

--- dms-domain/src/main/java/com/mlj/documents/DocumentRegisteredEventGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.mlj.documents;
-
-public class DocumentRegisteredEventGenerator {
-
-    public DocumentRegisteredEvent generateDocumentRegisteredEvent(String documentId, String title, String revision) {
-        return new DocumentRegisteredEvent(documentId, null, title, revision);
-    }
-}
